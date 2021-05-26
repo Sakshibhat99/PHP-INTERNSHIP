deeae69cdfd0ec58f04a0d2a804497cc56d2ae09
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-05-26 16:48:54 +0530

    Add files via upload

---
+++ Design.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="styles/stylesheet.css"
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">
+                <p><h1 align="center">HOSTEL MANAGEMENT SYSTEM </h1></p>
+            </div>
+            
+            <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="Login.php">Login</a></li>
+                    <li><a href="Registration.php">Registration</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">About Us</a></li>
+               
+                    
+                        
+                </ul>
+            </nav>
+            
+            <div id="content_area">
+                <?php echo $content;?>
+            </div>
+            
+            <div id="sidebar">
+                <img src="Images/PICTURE 3.png" class="imgRight"/>
+            </div>
+
+            <footer><p> All rights reserved @Sakshi Bhat</p></footer>
+            
+       
+    </body>
+</html>
