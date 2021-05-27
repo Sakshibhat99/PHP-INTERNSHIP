e8fc3d229d756f4ea22f302041d8f1e18d29ee1f
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-05-27 13:32:09 +0530

    Add files via upload

---
+++ DAY-1/index.php
@@ -0,0 +1,37 @@
+<?php
+$title = "Home";
+$content = '<img src="Images/PICTURE 2.jpg" class="imgLeft" />
+
+<p align="justify">
+    n some countries the word hostel is specifically used for the accommodation of student and travelers.
+    However in India and Pakistan, the hostel is believed to be a place of residence that a school, 
+    colleges or universities has, all hostels are supervised by the hostel wardens and other staff. 
+    The hostel generally consists hundreds of students. All of them make a group of students.
+    These students come from different ethical, social, geographical and economical background.
+</p>
+
+<img src="Images/PICTURE 7.png" class="imgRight"/>
+
+<p align="justify">
+    The hostel is a place where students stay for pursuing formal education away from their homes.
+    But the concept of hostel is not only limited to place of residence, hostel is a human practical laboratory. 
+    Therefore hostel is not simply a place for living it is a center of education. Students learn as much as
+    from their teachers as well as fellows during hostel stay. It enriches the understanding of the curriculum 
+    through analytical discussion among the students living in the hostels, and may contribute to character
+    building as well.Students in hostel not only learn the theoretical material they also learn how to enhance
+    their personal abilities and learn to live independently (Mishra, 1994).
+</p>
+
+<img src="Images/PICTURE 4.jpg" class="imgLeft" />
+
+<p align="justify">
+   
+There is a popular quotation “Times change people changes” it is best applicable for the hostel students.
+Hostel life going to change the way a student is, its effect on the personality behavior, thinking, 
+and dressing as well. In hostel students are surrounded by other students of about the same age as they are,
+all those students have different characteristics. 
+In hostel life all students have to adjust to the other students stayed in the hostel 
+</p>';
+
+include 'Design.php';
+?>
