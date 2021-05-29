26bb5672dc4153d67b58e38a93d8070fcff2a768
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-05-29 16:55:06 +0530

    TASK-2

---
+++ DAY-3/TASK 2/Marksheet.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Marksheet</title>
+        <style>
+      body {background: lightblue;} 
+      </style>
+
+    </head>
+   <body>
+  <h1 align="center">Marksheet</h1>
+    <form action="marks.php" method="POST">
+        <table align= "center">
+            <tr>
+                <td>Enter marks of English:-</td>
+                <td><input type="text" name="txt1"/></br></td>
+            </tr>
+            
+             <tr>
+                <td>Enter marks of Hindi:-</td>
+                <td><input type="text" name="txt2"/></br></td>
+            </tr>
+            
+             <tr>
+                <td>Enter marks of Math:-</td>
+                <td><input type="text" name="txt3"/></br></td>
+            </tr>
+            
+             <tr>
+                <td>Enter marks of Science:-</td>
+                <td><input type="text" name="txt4"/></br></td>
+            </tr>
+            
+             <tr>
+                <td>Enter marks of Social-Science:-</td>
+                <td><input type="text" name="txt5"/></br></td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="submit" value="Register"/>
+                    <input type="reset" value="Reset"/>
+                </td>
+            </tr>
+             
+    </form>
+</body>
+</html>
