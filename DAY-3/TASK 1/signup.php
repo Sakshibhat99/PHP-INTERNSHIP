532b8680506c61698abed4a5f7a6720533988efd
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-05-29 16:52:19 +0530

    DAY-3

---
+++ DAY-3/TASK 1/signup.php
@@ -0,0 +1,22 @@
+<?php
+
+ $a = $_POST['txt1'];
+  $b = $_POST['txt2'];
+   $c = $_POST['txt3'];
+   $d = $_POST['txt4'];
+    $e = $_POST['txt5'];
+     $f = $_POST['txt6'];
+      $g = $_POST['txt7'];
+       $h = $_POST['txt8'];
+         
+   echo"</br>First Name:- $a";
+    echo"</br>Last Name:- $b";
+     echo"</br>Date-Of-Birth:- $c";
+      echo"</br>Email:- $d";
+       echo"</br>Password:- $e";
+         echo"</br>Mobile-No.:- $f";
+           echo"</br>Address:- $g";
+             echo"</br>Gender:- $h";  
+           
+?>             
+           
