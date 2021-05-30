276c44d0306d6d339255b46c7881daf525472731
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-05-30 23:01:58 +0530

    TASK 3

---
+++ DAY-4/TASK 3/index.php
@@ -0,0 +1,83 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>TEMPLATE EXAMPLE 3</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div id="container">
+
+  <?php
+  
+  include 'Includes/header.php';
+  
+  ?>
+  
+  <div id="mainContent">
+  
+    <div class="blog">
+    
+      <h2 class="blogTitle">Wild City</h2>
+      
+      <img class="floatImgL" src="images/night_trip.jpg" alt="Night Trip" />
+      <p>Wild City is the best car racing game that you can play at any time anywhere, you will really enjoy
+          the competition by driving your suvs car, you'll have many different challenges, in this race 
+          you'll compete with a lot of racers, it's an amazing race car that enable you to feel like you 
+          drive in highway, you can also make a good pursuit by speeding you car to make a stunning results.</p>
+      
+          <p>we created this new game to give the racer feeling on the road, it's for all ages not only kids,
+          any one can play it, cause we knew that people love racing cars, when you installed this race game 
+          on your smartphone, tablet or any other android device, you'll see four choices (different Buttons :
+          play, scores, setting..) to start the game click on play Button then one touch on your device screen
+          you'll get started, after that tilt your device to steer away your car from other vehicles, use your
+          boost carefully for running fast than the other cars, as much as you get more stars and points, you 
+          have the chance to move the next levels.. So get it and don't forget to rate this game to encourage us. </p>
+      
+      <p>This new steering car game has a lot of new tracks and car game modes to offer too. Let us take a look at the unique features of this car race game.</p>
+
+      <h4>6 Racing Tracks</h4>
+      <ul>
+
+          <li>This car racing game has 2 day and night modes in 3 different tracks which makes it a total of 6 types of tracks.</li>
+    <li>The three-car racing tracks in this new game 2021 is:</li>
+    <li>Modern City Car Racing Track.</li>
+    <li>Landscape Car Racing Track.</li>
+    <li>Classical City Car Racing Game Track.</li>
+      </ul>
+      
+      
+      <h1>Wild City Features</h1>
+
+      <p>Main Feature of this Racing Games are:</p>
+      <ul>
+          <li>7 Unique Mountain Racing Asphalt Tracks.</li>
+     <li>Super Racing Cars to Unlock in Garage with Best Visual Effects.</li>
+     <li> Two Racing Modes: 1- Time Lapse 2- Racing with Opponent Cars.</li>
+     <li> Multiple Opponent Racers with high speed flame throwing Engines.</li>
+     <li> In Time Lapse, User will have to finish the mountain race before the time runout.</li>
+     <li> In Racing Mode, User will be racing with expert drivers of mountain tracks. Each Level with different challenges.</li>
+     <li> Dodge the opponent, block their ways to never let them pass your car.</li>
+     <li> High Level Artificial Intelligence involved.</li>
+
+     </ul>
+      </div>
+      
+    
+      <br class="clear" />
+          
+	</div><!-- end #mainContent -->
+    
+  <?php
+  
+include 'Includes/footer.php';
+  
+  ?>
+  
+</div><!-- end #container -->
+
+</body>
+</html>
