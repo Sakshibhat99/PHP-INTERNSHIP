c2a003312e9ac5b70e398681f5633203a1317265
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-05-30 22:55:46 +0530

    TASK 2

---
+++ DAY-4/TASK 2/index.php
@@ -0,0 +1,74 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>TEMPLATE EXAMPLE 2</title>
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Abel|Satisfy' rel='stylesheet' type='text/css'>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+	<?php
+        include 'Includes/header.php';
+        ?>
+
+	<div id="featured-content">
+		<div id="column1">
+                    <h2>Black-Eyed Susan</h2>
+                    <p><img src="images/IMAGE 2.jpg" width="300" height="150" alt="" /></p>
+			<p align="justify">This annual plant is scientifically known as Rudbeckia. This type of flower is often confused with Daisies.
+                            However, Black-eyed Susans are usually taller (as they can reach up to 4 feet in height), and they will
+                            bloom in late summer. They require lots of sunlight to survive and well-drained soil.</p>
+		
+			</div>
+		<div id="column2">
+                        <h2>Bird of Paradise</h2>
+			<p><img src="images/IMAGE 1.jpg" width="300" height="150" alt="" /></p>
+                        <p align="justify">Bird of Paradise,known as Strelitzia, is a perennial flower. Its yellow variety is very exotic! They need to be exposed to full sunlight; otherwise, they won’t thrive. </p> 
+		
+			</div>
+		<div id="column3">
+			<h2>Calendulas</h2>
+			<p><img src="images/IMAGE 3.jpg" width="300" height="150" alt="" /></p>
+                        <p align="justify">Calendulas are scientifically referred to as Calendula officinalis. They are not only edible flowers, 
+                            but they will also deter any pests away from the garden. Calendulas are known for being a very powerful flower with many health benefits as well! </p>
+		</div>
+	</div>
+	<div id="page">
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to Fashion Flowers </a></h2>
+				<div class="entry">
+					<p align="justify" ><strong>Fashion Flowers</strong>, believes that “Politeness is the flower of 
+                                            humanity.”.Flowers are one of nature’s most beautiful gifts to man and it would not be 
+                                            wrong to say that the blooming of a flower in itself symbolises life. plants produce 
+                                            flowers because they contain the male and female reproductive organs. Like human beings
+                                            plants also need to transfer Pollen from the male reproductive organs to the female ones.
+                                            Unlike human beings and animals, plants cannot move so they need to attract other creatures
+                                            which can do this job for them and for this purpose they produce flowers. Flowers have a 
+                                            delicate but complex structure and no two flowers are alike. Before blooming, a flower is
+                                            in the shape of a bud and is covered on the outside with scaly and rough sepals.</p>
+				
+				</div>
+			</div>
+		</div>
+		<!-- end #content -->
+		<?php
+                
+                        include 'Includes/sidebar.php';
+                
+                ?>
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<!-- end #page --> 
+</div>
+
+<?php
+
+                    include 'Includes/footer.php';
+?>
+
+</body>
+</html>
