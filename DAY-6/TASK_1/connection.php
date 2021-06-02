a888bf6219d0ebbdee18220ea9facc7e38f98c3c
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-06-02 22:22:52 +0530

    Database Connection

---
+++ DAY-6/TASK_1/connection.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$username = "root";
+$passwd = "";
+$dbname = "db_internship";
+//Connection function
+$connection = mysqli_connect($host, $username, $passwd, $dbname);
+
+$q = mysqli_query($connection,
+        "insert into tbl_user(user_name,user_gender,user_mobile)values('Sakshi','Female','1234567890')")
+        or die("Error". mysqli_error($connection));
+
+if($q)
+{
+    echo "<script>alert('Record Added!!');</script>";
+}
+
+?>
+        
