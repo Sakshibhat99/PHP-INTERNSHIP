3acdee4f818b9fc5fa7e80fc9a1a104b626e2aac
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-06-02 22:50:38 +0530

    Add files via upload

---
+++ DAY-6/TASK-2/logo.php
@@ -0,0 +1,8 @@
+<div id="heading">
+	    <div class="site_heading">
+	      <h2><span>Hostel</span> Management System</h2>	
+	    </div><!--close site_heading-->
+        <div class="site_slogan">	  
+		  <h2></h2>
+        </div><!--close site_slogan-->
+	  </div><!--close heading-->
---
+++ DAY-6/TASK-2/menu.php
@@ -0,0 +1,9 @@
+ <div id="menubar">
+          <ul id="menu">
+            <li class="current"><a href="index.php">Home</a></li>
+            <li><a href="#">Facilities</a></li>
+            <li><a href="#">Registration</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="about.php">About Us</a></li>
+          </ul>
+        </div>
