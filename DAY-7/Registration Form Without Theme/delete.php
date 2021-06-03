8f6e659c27540dddf5830b985e804d4306b7405c
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-06-03 13:04:10 +0530

    Delete Record(Student Table)

---
+++ DAY-7/Registration Form Without Theme/delete.php
@@ -0,0 +1,14 @@
+<?php
+//DB Connection
+$connection = mysqli_connect("localhost", "root", "" , "db_internship");
+
+
+$id = $_GET['deleteid'];
+
+$q = mysqli_query($connection, "delete from tbl_student where st_id= '{$id}'")or die(mysqli_error($connection) );
+
+if ($q)
+{
+    echo "<script>alert('Record Deleted');window.location='Display.php';</script>";
+}
+
