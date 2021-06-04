8b16a87322e7e13f0227ea197cdb287b95093d3a
Sakshi Bhat <84841645+Sakshibhat99@users.noreply.github.com>
2021-06-05 03:17:29 +0530

    Includes

---
+++ DAY-7/Registration Form With Theme/sidebar.php
@@ -0,0 +1,30 @@
+<div class="sidebar_container">  		  
+		<div class="sidebar">
+          <div class="sidebar_item">
+            <h2>News</h2>
+            <marquee direction=""down"  > Welcome to Our Website</marquee>
+			<h4>May2021</h4>
+            <p></p>
+		     
+           </div><!--close sidebar_item--> 
+         </div><!--close sidebar-->
+         
+	    <div class="sidebar">
+          <div class="sidebar_item">
+            <h2>Gallery</h2>
+		  	<h4>May 2021</h4>
+            <p></p>
+		     
+          </div><!--close sidebar_item--> 
+        </div><!--close sidebar-->  
+        
+		<div class="sidebar">
+          <div class="sidebar_item">
+            <h2>Accomodations</h2>
+			<h4>May 2021</h4>
+            <p></p>
+		
+          </div><!--close sidebar_item--> 
+        </div><!--close sidebar-->  
+      </div><!--close sidebar_container-->    
+	  
